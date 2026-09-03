44c62f9a93e34b4d3832d09a5496e95c9fd5f9af
Kandula Suvarna sri <suvarnasri710@gmail.com>
2026-09-03 20:56:45 +0530

    Add SHOW DATABASES command to SQL script
    Added a command to show existing databases after creating 'freelancer'.

---
+++ database/101_create_database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS freelancer;
+
+USE freelancer;
+
+show databases;
++--------------------+
+| Database           |
++--------------------+
+| freelancer         |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sampledb           |
+| sys                |
++--------------------+
